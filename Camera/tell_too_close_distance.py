0e2ae2512d4dfeb027a3941eda195d1e5febaa37
r-kitz <rose.kitz@tufts.edu>
2023-03-31 13:37:50 -0400

    Create tell_too_close_distance.py
    add simple code using distance sensor built-in to nicla vision camera
    prints ("stop!") and blinks red led when something is <150mm away from camera (in future modify to send command to brain to stop motors when something is too close)
    - future enhancement: make distance detection more sophisticated (i.e. sensor fusion w camera to sense if see hand and it is too close, and/or change distance to stop at based on speed robot is moving)

---
+++ Camera/tell_too_close_distance.py
@@ -0,0 +1,39 @@
+# VL53L1X ToF sensor basic distance measurement example.
+from machine import I2C
+from vl53l1x import VL53L1X
+import time
+import pyb
+
+# initialize distance (time-of-flight) sensor
+tof = VL53L1X(I2C(2))
+
+redLED = pyb.LED(1) # built-in red LED
+
+def blink_red():
+    redLED.on()
+    pyb.delay(10)
+    redLED.off()
+
+while True:
+    # get distance of board from closest object (using time-of-flight sensor)
+    dist = tof.read() # [mm]
+
+    # distance when an object becomes too close, so should stop robot
+    # (roughly tested so far, sensor gets some error as move away from things)
+    # updates could include doing some math based on speed to change STOP_MAX based on how fast robot is going to ensure sufficient time to stop motors
+    # (i.e. if going faster, increase STOP_MAX so send stop command sooner since robot is going faster and will cover more distance while stopping than if it were moving slower)
+    STOP_MAX = 150 # [mm]
+
+    # if robot is close to an obstacle, tell it to stop
+    # (for now just have print statement, for future send command to brain for brain to tell motors to stop)
+    if dist < STOP_MAX:
+        # replace this line below with message to brain to tell the motors to stop
+        print("Stop! Something is", dist, "mm away.")
+        # blink built-in red LED to visually indicate the robot is close to something
+        # (could replace with external LED later that is easier to see once the camera is mounted on the robot)
+        blink_red()
+    else:
+        pyb.delay(10) # since have 10ms delay if blink if object detected want same delay if not? so loop always has same time? or is this just making the loop less efficient?
+    #print(f"Distance: {tof.read()}mm")
+    time.sleep_ms(50)
+
